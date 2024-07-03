424a1930a54637f3b777f6ab0670ddcf339f5136
Rohit Yadav <66952591+irohitrao@users.noreply.github.com>
2024-07-03 10:32:23 +0530

    Delete Sonarqube Installation directory
    Removing Sonarqube directory from Master branch. Will create a separate branch and add later to master.

--- Sonarqube Installation/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "jenkins_server_ip" {
-  value = aws_instance.jenkins_server.public_ip
-}
-
-output "jenkins_admin_password" {
-  value = aws_instance.jenkins_server.user_data # Capture the user_data content
-  depends_on = [aws_instance.jenkins_server] # Ensure the instance is created before retrieving user_data
-  description = "Jenkins Admin Password"
-}
