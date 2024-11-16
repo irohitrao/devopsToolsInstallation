8d76f024435e84dc8ed244b9ea3aba55a583c561
Rohit Yadav <66952591+irohitrao@users.noreply.github.com>
2024-11-16 16:04:06 +0530

    Update secgrp.tf
    Added from UI

--- infra/secgrp.tf
+++ infra/secgrp.tf
@@ -3,7 +3,7 @@
 ## Ref: https://linuxhint.com/create-security-groups-aws-terraform/
 #########################################################
 
-resource "aws_security_group" "demo-sg" {
+resource "aws_security_group" "demo-sgp" {
   name = "sec-grp"
   description = "Allow HTTP and SSH traffic via Terraform"
 
